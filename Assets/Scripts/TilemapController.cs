e9798300110d6330afa730b2429083de81f380de
Kevin Wyckmans <kevinwyckmans@gmail.com>
2020-04-16 06:26:36 +0200

    add todos for future improvements

--- Assets/Scripts/TilemapController.cs
+++ Assets/Scripts/TilemapController.cs
@@ -15,8 +15,17 @@ public class TilemapController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //TODO: Switch this over from mousedown to an area around the player
+        //TODO: (optional) Give the area that is highlighted a border. This could potentially(!)
+        // be done by implementing a custom Tile class (https://docs.unity3d.com/Manual/Tilemap-ScriptableTiles-Example.html)
+        // that in GetTileData switches to a sprite with a border in the required direction for tiles at the 
+        // edge. If needed can even be scaled a bit to make it "stick out". Alternatively: don't do a border.
+        // Proposed solution would also allow for rounded borders.
+    
         if (Input.GetMouseButtonDown(0))
 		{
+            
+
             Debug.Log("Found a mousedown!");
 
             Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
